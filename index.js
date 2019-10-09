7039d6496137be84e6463d6304c7ddde1f6bb1d8
Aman Sultan Baig <41924807+AmanSultanBaig@users.noreply.github.com>
2019-10-09 22:32:44 +0500

    Update index.js
    short implementation of array

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var arr = [12, 1000, 34, 170, 190, 67, 89, 90, 66, 74, 100, 230];
 
+// Long way to do it 
 let largestNum = 0;
 let secondNum = 0;
 let thirdNum = 0;
@@ -14,7 +15,10 @@ for (let i = 0; i <= arr.length; i++) {
         thirdNum = arr[i]
     }
 }
-
-console.log(largestNum)
-console.log(secondNum)
 console.log(thirdNum)
+
+// short way to do it
+let getSorted = arr.sort((a, b) => {
+    return b - a;
+});
+console.log(getSorted[2]);
